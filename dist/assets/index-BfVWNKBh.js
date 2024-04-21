0a35e3931215e710aa6fae5e318b2a33c6b966f0
jurjees23 <s.jurjeesahamed555@gmail.com>
2024-04-21 07:13:17 +0400

    build moved

---
+++ dist/assets/index-BfVWNKBh.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-TPODpp2I.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"About Page"})});export{t as default};
---
+++ dist/assets/index-Wcnr6ch_.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-TPODpp2I.js";const s=()=>r.jsx("div",{children:r.jsx("h1",{children:"Dashboard Page"})});export{s as default};
